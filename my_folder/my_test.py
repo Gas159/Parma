139f65fa8314670729d5607c562902fd84238a47
Gas159 <pankot222@gmail.com>
2023-01-31 15:06:36 +0500

    delete unnecessary

--- my_folder/my_test.py
+++
@@ -1,8 +0,0 @@
-def find_even_index(arr):
-    # your code here
-    for i, e in enumerate(arr):
-        print(i, e)
-
-    return -1
-
-print(find_even_index([1, 2, 3, 4, 3, 2, 1]))
--- my_folder/oop/Monosostoyanie.py
+++
@@ -1,5 +0,0 @@
-class ThreadData: # thread - поток
-    __shared_attrs = {'name': 'thread1', 'date': {}, 'id': 1}
-
-    def __init__(self):
-        self.__dict__ = self.__shared_attrs
