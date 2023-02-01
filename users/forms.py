a01f77c00e0c1c2073a9caffa94d7a3e881bb087
Gas159 <pankot222@gmail.com>
2023-02-01 15:39:18 +0500

    finish step 8 fix 18

--- users/forms.py
+++ users/forms.py
@@ -5,5 +5,5 @@ from django.contrib.auth.forms import UserCreationForm
 class RegisterUserForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ['username', 'first_name']  # fields = '__all__'
+        fields = ['username', 'first_name', 'last_name']  # fields = '__all__'
         ordering = ['-date_joined']
