86a6d848c2b13949640117165520edbde7dd71cf
Gas159 <pankot222@gmail.com>
2023-02-01 15:01:15 +0500

    finish step 8 fix 16

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,10 +1,8 @@
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext as _
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth import logout
 from django.views.generic import TemplateView
 
 
@@ -22,9 +20,16 @@ class UserLoginView(SuccessMessageMixin, LoginView):
 
 
 class UserLogoutView(SuccessMessageMixin, LogoutView):
-    success_message = _('Successfully logout')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.info(request, _('You are logged out'))
+        return super().dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
-        messages.warning(self.request, self.success_message)
         return reverse_lazy('home')
+
+    # def get_success_url(self):
+    #     messages.warning(self.request, _('Successfully logout'))
+    #     return reverse_lazy('home')
 
