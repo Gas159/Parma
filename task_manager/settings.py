7c0eab3dc7f154031e7fa0e9f229d51772d445cc
Gas159 <pankot222@gmail.com>
2023-02-02 08:52:22 +0500

    test error fix 3

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -71,8 +71,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-if DEBUG is not True:
-    DATABASES = os.getenv('DATABASE_URL')
+
+if os.getenv('DATABASE_URL'):
     db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES['default'].update(db_from_env)
 
