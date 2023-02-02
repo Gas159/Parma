974426b9f0af27270debcf88974e472fb8bd2801
Gas159 <pankot222@gmail.com>
2023-02-02 08:32:14 +0500

    test error fix 1

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from tasks.filters import TaskFilter
 from django.shortcuts import redirect
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 
