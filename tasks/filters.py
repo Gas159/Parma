b12d7085f326461325ae097f0335dab08a706aab
Gas159 <pankot222@gmail.com>
2023-02-02 06:34:53 +0500

    user auth fix 5

--- tasks/filters.py
+++ tasks/filters.py
@@ -1,5 +1,7 @@
 import django_filters
 from labels.models import Labels
+from users.models import Users
+from statuses.models import Status
 from tasks.models import Task
 from django import forms
 from django.utils.translation import gettext_lazy as _
@@ -11,6 +13,17 @@ class TaskFilter(django_filters.FilterSet):
             return queryset.filter(author=self.request.user)
         return queryset
 
+    status = django_filters.ModelChoiceFilter(
+        label=_('Status'),
+        queryset=Status.objects.all(),
+        widget=forms.Select(attrs={'class': 'm-1'})
+    )
+    executor = django_filters.ModelChoiceFilter(
+        label=_('Executor'),
+        queryset=Users.objects.all(),
+        widget=forms.Select(attrs={'class': 'm-1'})
+    )
+
     labels = django_filters.ModelChoiceFilter(
         queryset=Labels.objects.all(),
         widget=forms.Select(attrs={
@@ -19,7 +32,7 @@ class TaskFilter(django_filters.FilterSet):
 
     self_task = django_filters.BooleanFilter(
         method='show_own_task',
-        widget=forms.CheckboxInput,
+        widget=forms.CheckboxInput(attrs={'class': 'm-2'}),
         label=_('Show own tasks'),
     )
 
