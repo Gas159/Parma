e19f6a7f9276fc465376b68603dc93eccae99247
gas <pankot222@gmail.com>
2024-03-01 19:29:05 +0500

    fad

--- tools/models.py
+++ tools/models.py
@@ -8,11 +8,10 @@ class Tool(models.Model):
     name = models.CharField(max_length=222, unique=True, verbose_name=_('Tool name'))
     description = models.TextField(max_length=333, verbose_name=_('Description'))
     workplace = models.ForeignKey(Workplace, on_delete=models.PROTECT,
-                                       null=True, blank=True, verbose_name=_('Workplace'))
-    in_supply = models.CharField(max_length=222, null=True, blank=True,  default='В наличии')
+                                  null=True, blank=True, verbose_name=_('Workplace'))
+    in_supply = models.CharField(max_length=222, null=True, blank=True, default='В наличии')
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     update_at = models.DateTimeField(auto_now=True, null=True)
-
 
     def __str__(self):
         return self.name
