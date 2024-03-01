9f2d62d54e80196c046d69542041ab2651fde75a
Sahapov Rinat <100280901+Gas159@users.noreply.github.com>
2024-03-01 08:49:44 +0500

    Update models.py
    Changed default for workplace

--- tools/models.py
+++ tools/models.py
@@ -6,9 +6,9 @@ from workplaces.models import Workplace
 
 class Tool(models.Model):
     name = models.CharField(max_length=222, unique=True, verbose_name=_('Tool name'))
-    description = models.TextField(max_length=333, blank=True, verbose_name=_('Description'))
+    description = models.TextField(max_length=333, verbose_name=_('Description'))
     workplace = models.ForeignKey(Workplace, on_delete=models.PROTECT,
-                                       null=True, verbose_name=_('Workplace'))
+                                       null=True, default='На складе',verbose_name=_('Workplace'))
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     update_at = models.DateTimeField(auto_now=True, null=True)
 
