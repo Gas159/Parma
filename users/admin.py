f6aea2a4201dc3f3101ef5c3820a9a6486640a3a
gas <pankot222@gmail.com>
2024-03-28 15:43:06 +0500

    upd admin

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,5 @@ from users.models import Users
 @admin.register(Users)
 class UserAdmin(admin.ModelAdmin):
     search_fields = ['username']
-    list_display = ('id', 'first_name', 'last_name', 'date_joined', 'password', 'email')
+    # list_display = ('id', 'first_name', 'last_name', 'date_joined', 'password', 'email')
+    list_display = [f.name for f in Users._meta.fields]
